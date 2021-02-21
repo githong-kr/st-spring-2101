f0a80f4be0152a2d224ace2e5bf4718eff274002
githong@kakao.com <rkfaorl12!@>
2021-02-21 17:07:29 +0900

    log added

--- src/main/java/com/example/demo/part/com/controller/OneController.java
+++ src/main/java/com/example/demo/part/com/controller/OneController.java
@@ -1,7 +1,9 @@
 package com.example.demo.part.com.controller;
 
 
+import ch.qos.logback.classic.Logger;
 import com.example.demo.part.com.service.OneService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/one")
 public class OneController {
 
+    private static Logger log = (Logger) LoggerFactory.getLogger(OneController.class);
+
     @Autowired
     OneService service;
 
     public OneController(OneService service) {
+        log.info("test logger");
+        log.info("test debug logger");
         this.service = service;
     }
 
